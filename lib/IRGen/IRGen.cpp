5d4a74559e71c65993ed71f7b9b5c5e2dbe8e842
Saleem Abdulrasool <compnerd@compnerd.org>
2016-05-16 12:12:09 -0700

    getSection() returns a StringRef now
    Update to account for the fact that the return type has changed from
    `const char *` to `StringRef`.

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -316,10 +316,10 @@ static bool needsRecompile(StringRef OutputFilename, ArrayRef<uint8_t> HashData,
   if (!ObjectFile)
     return true;
 
-  const char *HashSectionName = HashGlobal->getSection();
+  StringRef HashSectionName = HashGlobal->getSection();
   // Strip the segment name. For mach-o the GlobalVariable's section name format
   // is <segment>,<section>.
-  if (const char *Comma = ::strchr(HashSectionName, ','))
+  if (const char *Comma = ::strchr(HashSectionName.data(), ','))
     HashSectionName = Comma + 1;
 
   // Search for the section which holds the hash.
