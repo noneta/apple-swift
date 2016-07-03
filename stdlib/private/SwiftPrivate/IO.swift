1e013246f38bfdab180f8d24be447fe327cc12f9
Saleem Abdulrasool <compnerd@compnerd.org>
2016-07-03 14:43:56 -0700

    HACK: disable protocol conformance pending ABI resolution
    The COFF execution model requires indirect addressing for symbols from external
    modules.  Because _TMps12OutputStream (aka Protocol Descriptor for
    Swift.OutputStream) resides in swiftCore.dll, we need an indirect addressing of
    it rather than the direct one currently generated in the protocol conformance
    table.

--- stdlib/private/SwiftPrivate/IO.swift
+++ stdlib/private/SwiftPrivate/IO.swift
@@ -81,7 +81,7 @@ public struct _FDInputStream {
   }
 }
 
-public struct _Stderr : OutputStream {
+public struct _Stderr /* : OutputStream */ {
   public init() {}
 
   public mutating func write(_ string: String) {
@@ -91,7 +91,7 @@ public struct _Stderr : OutputStream {
   }
 }
 
-public struct _FDOutputStream : OutputStream {
+public struct _FDOutputStream /* : OutputStream */ {
   public let fd: CInt
   public var isClosed: Bool = false
 
